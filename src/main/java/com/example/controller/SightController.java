3a8b8417ca2ac6b908fe59ee05d02e588ca9271d
warren00857020 <warren5606@gmail.com>
2025-05-15 15:48:16 +0800

    在搞，不要用Query String

--- src/main/java/com/example/controller/SightController.java
+++ src/main/java/com/example/controller/SightController.java
@@ -24,10 +24,10 @@ public class SightController {
     private SightService sightService;
 
     //爬蟲進DB
-    @GetMapping("/setSights")//
-    public ResponseEntity<List<SightRequest>> putSights(@RequestParam String zone) throws IOException {
+    @GetMapping("/setSights/{Zone}")//
+    public ResponseEntity<List<SightRequest>> putSights(@PathVariable("Zone") String Zone) throws IOException {
         KeelungSightsCrawler crawler = new KeelungSightsCrawler();
-        List<SightRequest> sights = crawler.getItems(zone);
+        List<SightRequest> sights = crawler.getItems(Zone);
         for (SightRequest s: sights) {
             sightService.createSight(s);
         }
